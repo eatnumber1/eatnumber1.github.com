44af99cd7c7ee09e82ae927814a676c135c9b131
Russell Harmon <russ@eatnumber1.com>
2014-05-25 16:44:12 -0400

    Automatically generated site on Sun May 25 16:44:12 EDT 2014

--- js/latitude.js
+++
@@ -1,1 +0,0 @@
-function doLatitude(e,r){$.param({user:e,type:"json"});$.yql("SELECT features.properties.reverseGeocode FROM json WHERE url=@url",{url:"http://www.google.com/latitude/apps/badge/api?"+$.param({user:e,type:"json"})},function(e){e=e.query.results,null!=e&&r.replaceWith(e.json.features.properties.reverseGeocode)},function(e,t,o){r.replaceWith(t+": "+o)})}
